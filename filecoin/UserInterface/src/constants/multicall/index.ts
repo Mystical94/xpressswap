a3f299f9d5651612c5b81159a81973a86a9eaac6
Mystical94 <53875398+Mystical94@users.noreply.github.com>
2023-02-19 20:18:26 +0530

    UserInterface Filecoin Network
    user interface of xpressswap for filecoin network

---
+++ filecoin/UserInterface/src/constants/multicall/index.ts
@@ -0,0 +1,13 @@
+import { ChainId } from '@uniswap/sdk';
+import MULTICALL_ABI from './abi.json';
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
+  [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
+  [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
+  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e',
+  [ChainId.FILECOIN]: '0x0c0d088a6fe7c65754d821eb94bce29c2cfb0d1d',
+};
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS };
